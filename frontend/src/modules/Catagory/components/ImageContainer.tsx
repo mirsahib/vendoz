e58728bef0ac528b1070e3e5ac78e8c002a19f43
Mir Sahib <mirsahib24@gmail.com>
2023-05-05 21:57:13 +0600

    :hammer: initial catagory section done

---
+++ frontend/src/modules/Catagory/components/ImageContainer.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import React from "react";
+
+interface IImgContainer{
+    name: string;
+    width:number;
+    height: number;
+    url:string
+
+}
+
+export default function ImageContainer({name,width,height,url}:IImgContainer) {
+	return (
+		<div className="relative m-3 overflow-hidden">
+			<Image
+				src={url}
+				className="hover:scale-110 ease-in duration-500"
+				width={width}
+				height={height}
+				alt="men"
+			/>
+
+			<div className="top-1/2 left-[40%] text-gray-700 absolute">
+				<button className="p-3 rounded bg-blue-600 text-white  hover:bg-white hover:text-gray-800">
+					{name}
+				</button>
+			</div>
+		</div>
+	);
+}
--- frontend/src/modules/Catagory/index.tsx
+++ frontend/src/modules/Catagory/index.tsx
@@ -1,68 +1,58 @@
 import Image from "next/image";
 import React from "react";
+import ImageContainer from "./components/ImageContainer";
 
 export default function Catagory() {
 	return (
 		<section className="flex">
 			<div className="flex basis-1/2">
 				<div className="flex flex-col basis-1/2">
-					<div className="p-3">
-						<Image
-							src={"/men-cat.jpg"}
-							width={320}
-							height={260}
-							alt="men"
-						/>
-					</div>
-					<div className="p-3">
-						<Image
-							src={"/women-cat.jpg"}
-							width={320}
-							height={260}
-							alt="men"
-						/>
-					</div>
+					<ImageContainer
+						name="MEN"
+						width={320}
+						height={260}
+						url="/men-cat.jpg"
+					/>
+					<ImageContainer
+						name="MEN"
+						width={320}
+						height={260}
+						url="/women-cat.jpg"
+					/>
 				</div>
 				<div className="flex basis-1/2">
-					<div className="p-2">
-						<Image
-							src={"/women-cat-2.jpg"}
-							width={320}
-							height={550}
-							alt="men"
-						/>
-					</div>
+					<ImageContainer
+						name="MEN"
+						width={320}
+						height={560}
+						url="/women-cat-2.jpg"
+					/>
 				</div>
 			</div>{" "}
 			{/* row one */}
 			<div className="flex flex-col basis-1/2">
 				<div className="flex flex-row">
-					<div className="p-3">
-						<Image
-							src={"/men-cat.jpg"}
-							width={320}
-							height={550}
-							alt="men"
-						/>
-					</div>
-					<div className="p-3">
-						<Image
-							src={"/access.jpg"}
-							width={320}
-							height={550}
-							alt="men"
-						/>
-					</div>
+					<ImageContainer
+						name="MEN"
+						width={320}
+						height={260}
+						url="/men-cat.jpg"
+					/>
+
+					<ImageContainer
+						name="MEN"
+						width={320}
+						height={260}
+						url="/access.jpg"
+					/>
 				</div>
 				<div>
-					<div className="p-3">
-						<Image
-							src={"/shoe-cat.jpg"}
-							width={640}
-							height={270}
-							alt="men"
-						/>
-					</div>
+					<ImageContainer
+						name="MEN"
+						width={640}
+						height={270}
+						url="/shoe-cat.jpg"
+					/>
 				</div>
 			</div>{" "}
 			{/* row 2 */}
