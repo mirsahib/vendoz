2a412bac33393bdea32ee953860a26c3cb78b9d2
Mir Sahib <mirsahib24@gmail.com>
2023-05-05 19:08:26 +0600

    :hammer: initial hero section done

--- frontend/src/modules/Hero/index.tsx
+++ frontend/src/modules/Hero/index.tsx
@@ -3,24 +3,30 @@ import React from "react";
 
 export default function Hero() {
 	return (
-		<section className="w-full relative h-[85vh]">
+		<section className="lg:w-full relative h-[80vh] ">
 			<div className="-z-10">
 				<Image
-					src={"/shopping.jpg"}
+					src={"/banner.jpg"}
 					fill={true}
 					style={{
 						objectFit: "cover",
-						objectPosition: "center",
+						objectPosition: "center", 
 					}}
 					alt="hero"
+					quality={90}
+					priority={true}
+					
 				/>
 			</div>
-			<div className="flex relative">
-				<h1>Vendoz</h1>
-				<p>
+			<div className="flex flex-col top-[15%] lg:left-[12%] sm:left-[5%] relative ">
+				<h1 className="text-blue-600 text-3xl font-mono font-bold">Vendoz</h1>
+				<p className=" text-gray-100 font-semibold text-lg mb-6">
 					Lorem ipsum dolor sit amet consectetur adipisicing elit.
 					Tempore
 				</p>
+				<div className="">
+					<button className="p-3 bg-blue-600 rounded hover:bg-blue-500 text-white ">Shop Now</button>
+				</div>
 			</div>
 		</section>
 	);
