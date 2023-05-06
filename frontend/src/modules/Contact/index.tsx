da683a095877cc2d5c029a54bb798c36b823dbdd
Mir Sahib <mirsahib24@gmail.com>
2023-05-06 09:52:29 +0600

    :hammer: initial contact section done

--- frontend/src/modules/Contact/index.tsx
+++ frontend/src/modules/Contact/index.tsx
@@ -2,13 +2,49 @@ import React from "react";
 
 export default function Contact() {
 	return (
-		<section className="flex bg-blue-500 my-8">
+		<section className="flex bg-blue-600 my-8 py-2">
 			<div className="flex justify-between w-[90%] m-auto">
-				<div>
-					<h3>Be touch with us</h3>
+				<div className="flex items-center basis-1/4 text-white">
+					<h3 className=" font-medium">BE TOUCH WITH US</h3>
 				</div>
-				<div>Search</div>
-				<div>Social</div>
+				<div className="flex basis-1/2">
+					<input
+						type="text"
+						placeholder="Enter your email"
+						className=" basis-3/4 rounded p-2  focus:outline-none h-10 "
+					/>
+					<button className=" basis-1/4 bg-black rounded text-white">
+						JOIN US
+					</button>
+				</div>
+				<div className="flex justify-end basis-1/4">
+					<ul className="flex items-center">
+						<li className="mx-5 text-white">
+							<i
+								className="fab fa-facebook"
+								aria-hidden="true"
+							></i>
+						</li>
+						<li className="mx-5 text-white">
+							<i
+								className="fab fa-twitter"
+								aria-hidden="true"
+							></i>
+						</li>
+						<li className="mx-5 text-white">
+							<i
+								className="fab fa-instagram"
+								aria-hidden="true"
+							></i>
+						</li>
+						<li className="mx-5 text-white">
+							<i
+								className="fab fa-pinterest"
+								aria-hidden="true"
+							></i>
+						</li>
+					</ul>
+				</div>
 			</div>
 		</section>
 	);
