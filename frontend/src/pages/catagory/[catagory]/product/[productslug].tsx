1962cf0bfcb1a4dcd7f9602ba16f4a47173ce4a9
Mir Sahib <mirsahib24@gmail.com>
2023-05-07 01:16:47 +0600

    :hammer: initial single product done

--- frontend/src/pages/catagory/[catagory]/product/[productslug].tsx
+++ frontend/src/pages/catagory/[catagory]/product/[productslug].tsx
@@ -1,7 +1,63 @@
-import React from 'react'
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React from "react";
+import { product } from "@/mock/data";
 
-export default function Product() {
-  return (
-    <div>[productslug]</div>
-  )
+
+const data = product[0];
+
+export default function SingleProduct() {
+	const router = useRouter();
+	const { catagory, productslug } = router.query;
+	console.log(productslug);
+	return (
+		<>
+			<Head>
+				<title>Product Name</title>
+			</Head>
+			<main>
+				<div className="bg-slate-100 py-1">
+					<ul className="flex w-[75%] text-gray-700 text-sm m-auto">
+						<li>
+							<Link href={"/"}>HOME/</Link>
+						</li>
+						<li>
+							<Link href={"/"}>MEN/</Link>
+						</li>
+						<li>
+							<Link href={"/"}>T-Shirt</Link>
+						</li>
+					</ul>
+				</div>
+				{/* nav section */}
+				<section className="w-[80%] py-10 m-auto">
+					<div className="flex flex-row justify-around">
+						<div className="bg-red-100">
+							<Image
+								src={data.img}
+								width={360}
+								height={250}
+								alt="tshirt"
+							/>
+						</div>
+						{/* image container */}
+						<div className="flex flex-col justify-center ">
+							<h3 className="font-semibold text-gray-800 text-2xl w-[85%]">{data.name}</h3>
+							<h3 className="my-5 text-lg font-semibold text-gray-800">$ {data.price}</h3>
+							<div className="flex justify-around">
+								<div>1</div>
+								<div>
+									<button className="bg-blue-700 text-white p-3 rounded">ADD TO CART</button>
+								</div>
+							</div>
+						</div>
+						{/* product details */}
+					</div>
+				</section>
+        
+			</main>
+		</>
+	);
 }
