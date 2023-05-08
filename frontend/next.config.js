a1709875c7b98bdeb372646fb966619cf4a22c88
Mir Sahib <mirsahib24@gmail.com>
2023-05-08 20:09:58 +0600

    :wrench: Add image domain

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,13 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images:{
+    domains:['localhost']
+  },
+  env:{
+    REACT_API_BASE_URL: 'http://localhost:1337'
+  }
 }
 
 module.exports = nextConfig
