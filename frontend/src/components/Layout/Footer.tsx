6fd6f8fcab7f1bd8295e8e2eb27947452be6ceea
Mir Sahib <mirsahib24@gmail.com>
2023-05-11 19:45:22 +0600

    :iphone: Footer responsiveness done

--- frontend/src/components/Layout/Footer.tsx
+++ frontend/src/components/Layout/Footer.tsx
@@ -3,11 +3,11 @@ import React from "react";
 export default function Footer() {
 	return (
 		<footer className="flex w-[90%] m-auto py-8">
-			<div className="flex items-center basis-1/2">
-				<h3 className="text-blue-600 text-2xl font-semibold mr-1">
+			<div className="flex flex-col lg:flex-row sm:flex-row items-start lg:items-center sm:items-center basis-1/2">
+				<h3 className="text-blue-600 text-base lg:text-2xl sm:text-lg font-semibold lg:mr-1 sm:mr-1">
 					Vendoz
 				</h3>
-				<p className="text-sm text-gray-700">
+				<p className="text-xs lg:text-sm sm:text-sm text-gray-700">
 					© Vendoz {new Date().getFullYear()}. All Rights Reserved
 				</p>
 			</div>
