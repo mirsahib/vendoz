a66f20ba6f194ec6abc1de38dd3b7ae7b0ad8eda
Mir Sahib <mirsahib24@gmail.com>
2023-05-11 11:57:27 +0600

    :recycle: fixing card responsiveness

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -2,27 +2,34 @@ import { IProduct } from "@/lib/types";
 import Image from "next/image";
 import React from "react";
 import { urlBuilder } from "@/util/UrlBuilder";
-interface ICard{
+interface ICard {
 	props: IProduct["attributes"];
 }
 
 export default function Card({ props }: ICard) {
-	const truncate = (input:string|null)=>{
-		return input!=null && input.length>45? `${input.substring(0,45)}...`:input
-	}
+	const truncate = (input: string | null) => {
+		return input != null && input.length > 45
+			? `${input.substring(0, 45)}...`
+			: input;
+	};
 	return (
-		<div className="bg-slate-100 relative m-2 p-2 h-[32em] ">
-			<Image
-				src={urlBuilder(props.img?.data?.attributes?.url)}
-				width={320}
-				height={250}
-				alt="men"
-			/>
-
-			<div className="top-[5%] left-[80%] text-gray-700 absolute">
-				<button className=" text-gray-800 hover:text-white">
-					<i className="far fa-heart bg-slate-100 hover:bg-blue-600 p-3 rounded-full"></i>
-				</button>
+		<div className="flex flex-col justify-evenly bg-slate-100 relative m-2 pb-5 h-96 shadow-md hover:shadow-lg">
+			<div className="relative w-[100%] h-[90%] mx-auto overflow-hidden">
+				<Image
+					src={urlBuilder(props.img?.data?.attributes?.url)}
+					fill
+					alt="men"
+					style={{ objectFit: "cover", objectPosition: "center" }}
+				/>
+				<div className="flex justify-center items-center absolute top-5 right-5 w-8 h-8  bg-slate-100 hover:bg-blue-700 hover:text-white rounded-full">
+					<button onClick={(e)=>{
+						e.preventDefault();
+						e.stopPropagation();
+						console.log('click')
+					}} className="flex justify-center items-center ">
+						<i className="far fa-heart"></i>
+					</button>
+				</div>
 			</div>
 			<div className="px-5">
 				<div className="flex flex-col my-2">
