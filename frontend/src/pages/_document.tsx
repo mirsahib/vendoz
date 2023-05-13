a396a7eabc918d38f5d1ff117d2e752d5a21a0b4
Mir Sahib <mirsahib24@gmail.com>
2023-05-13 09:49:36 +0600

    :arrow_up: Upgrade fontawesome

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -6,10 +6,8 @@ export default function Document() {
 			<Head>
 				<link
 					rel="stylesheet"
-					href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css"
-					integrity="sha512-q3eWabyZPc1XTCmF+8/LuE1ozpg5xxn7iO89yfSOd5/oKvyqLngoNGsx8jq92Y8eXJ/IRxQbEC+FGSYxtk2oiw=="
-					crossOrigin="anonymous"
-					referrerPolicy="no-referrer"
+					href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+					
 				/>
 
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
