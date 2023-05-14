1255fd5e7458265090bd2af2b946c6f23122909d
Mir Sahib <mirsahib24@gmail.com>
2023-05-14 07:06:33 +0600

    :hammer: Hide slide on mobile view

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -3,10 +3,10 @@ import React from "react";
 
 export default function Hero() {
 	return (
-		<section className="lg:w-full relative h-[80vh] ">
+		<section className="hidden lg:block sm:hidden md:block lg:w-full relative h-[80vh]  ">
 			<div className="-z-10">
 				<Image
-					src={"/banner.jpg"}
+					src={"/images/banner-1.webp"}
 					fill={true}
 					style={{
 						objectFit: "cover",
@@ -18,7 +18,7 @@ export default function Hero() {
 					
 				/>
 			</div>
-			<div className="flex flex-col top-[15%] left-[10%] lg:left-[15%] sm:left-[5%] absolute">
+			{/* <div className="flex flex-col top-[15%] left-[10%] lg:left-[15%] sm:left-[5%] absolute">
 				<h1 className="text-blue-600 text-base lg:text-3xl sm:text-2xl font-mono font-bold">Vendoz</h1>
 				<p className=" text-gray-100 font-semibold text-sm lg:text-lg sm:text-base mb-6">
 					Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -27,7 +27,7 @@ export default function Hero() {
 				<div className="">
 					<button className="p-2 lg:p-3 sm:p-3 bg-blue-600 rounded hover:bg-blue-500 text-white text-sm lg:text-xl sm:text-base">Shop Now</button>
 				</div>
-			</div>
+			</div> */}
 		</section>
 	);
 }
