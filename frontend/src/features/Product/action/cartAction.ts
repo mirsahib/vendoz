8d78723e2f96672606421a0dc034546c4ed59927
Mir Sahib <mirsahib24@gmail.com>
2023-05-17 12:17:20 +0600

    :hammer: Add redux action

---
+++ frontend/src/features/Product/action/cartAction.ts
@@ -0,0 +1,42 @@
+import { IProduct } from "@/lib/types"
+import { AppDispatch } from "@/store"
+import { addToCart, decreaseCart, increaseCart, removeFromCart } from "@/store/cart"
+
+const addItem= (product:IProduct)=>{
+    console.log('dispatch')
+    return (dispatch:AppDispatch)=>{
+        dispatch(addToCart(product))
+    }
+}
+const removeItem = (product:IProduct|null)=>{
+    return (dispatch:AppDispatch)=>{
+        if(product!=null){
+            dispatch(removeFromCart(product))
+        }
+    }
+}
+const increaseItem=(product:IProduct|null)=>{
+    console.log('increaseItem')
+    return (dispatch:AppDispatch)=>{
+        if(product!=null){
+            console.log('increase product')
+            dispatch(increaseCart(product))
+        }
+
+    }
+}
+const decreaseItem=(product:IProduct|null)=>{
+    return (dispatch:AppDispatch)=>{
+        if(product!=null){
+            dispatch(decreaseCart(product))
+        }
+
+    }
+}
+export {
+    addItem,
+    removeItem,
+    increaseItem,
+    decreaseItem,
+}
+ 
