2771e627237c9923974e00189e32e87e11928df5
Mir Sahib <mirsahib24@gmail.com>
2023-05-20 19:33:02 +0600

    :recycle: Modify login api

--- frontend/src/pages/api/login.ts
+++ frontend/src/pages/api/login.ts
@@ -1,51 +1,35 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-
+import type {
+	AuthSuccessResponse,
+	ApiErrorResponse,
+	IAuthPayload,
+} from "@/lib/types";
+import makeApiCall from "@/util/makeApiCall";
+import errorMessage from "@/util/errorMessage";
 interface ApiRequest extends NextApiRequest {
-	body: {
-		email: string;
-		password: string;
-	};
-}
-interface StrapiResponse {
-	jwt?: string;
-	user?: {
-		id: number;
-		username: string;
-		email: string;
-		provider: string;
-		confirmed: boolean;
-		blocked: boolean;
-		createdAt: Date;
-		updatedAt: Date;
-	};
-	data?: any;
-	error?: {
-		status: number;
-		name: string;
-		message: string;
-		detail: Object;
-	};
+	body: IAuthPayload;
 }
 
 export default async function handler(req: ApiRequest, res: NextApiResponse) {
 	const { email, password } = req.body;
 	try {
-		const payload = { identifier: email, password: password };
-		const response = await fetch(
-			`${process.env.REACT_API_URL}/auth/local`,
-			{
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify(payload),
-			}
-		);
-		const data = (await response.json()) as StrapiResponse;
-		res.status(200).json({ data });
+		const payload: IAuthPayload = { identifier: email, password: password };
+		const data = await makeApiCall<
+			AuthSuccessResponse,
+			ApiErrorResponse,
+			IAuthPayload
+		>("/auth/local", "POST", payload);
+
+		if (data && "error" in data) {
+			res.status(400).json({ ...data });
+		} else {
+			res.status(200).json({ user: data.user });
+		}
 	} catch (error) {
-		console.log("login handler", error);
-		res.status(400).send(error);
+		const ErrorResponse = errorMessage(error);
+		res.status(500).json({
+			...ErrorResponse,
+		});
 	}
 }
