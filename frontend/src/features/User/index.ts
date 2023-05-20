ab815e03f74e483a4aae09c3e0364c89b3952181
Mir Sahib <mirsahib24@gmail.com>
2023-05-20 19:30:54 +0600

    :hammer: Add User feature

---
+++ frontend/src/features/User/index.ts
@@ -0,0 +1,16 @@
+import Login from "./container/Login";
+import Register from "./container/Register";
+
+const Container = {
+    LoginContainer: Login,
+    RegisterContainer: Register
+}
+
+const Component = {}
+
+const User = {
+    Container: Container,
+    Component: Component
+}
+
+export default User
