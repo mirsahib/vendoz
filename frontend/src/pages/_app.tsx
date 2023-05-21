c5d898fd543ec9d8d01116dea0ecf56f8f1313ab
Mir Sahib <mirsahib24@gmail.com>
2023-05-21 11:37:04 +0600

    :zap: Add performant fonts

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -3,12 +3,17 @@ import store from "@/store";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
+import { Comme } from "next/font/google";
+
+const comme = Comme({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<Provider store={store}>
 			<MainLayout>
-				<Component {...pageProps} />
+				<main className={comme.className}>
+					<Component {...pageProps} />
+				</main>
 			</MainLayout>
 		</Provider>
 	);
--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -9,13 +9,6 @@ export default function Document() {
 					href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
 					
 				/>
-
-				<link rel="preconnect" href="https://fonts.googleapis.com" />
-				<link rel="preconnect" href="https://fonts.gstatic.com" />
-				<link
-					href="https://fonts.googleapis.com/css2?family=Comme:wght@400;500;600;700&display=swap"
-					rel="stylesheet"
-				/>
 			</Head>
 			<body>
 				<Main />
