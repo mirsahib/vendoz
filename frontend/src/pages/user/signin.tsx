7619630f671acf97d685610c88d9466085d72037
Mir Sahib <mirsahib24@gmail.com>
2023-05-21 09:35:27 +0600

    :hammer: Add lazy loading

--- frontend/src/pages/user/signin.tsx
+++ frontend/src/pages/user/signin.tsx
@@ -1,6 +1,16 @@
 import Head from "next/head";
 import React, { useState } from "react";
 import User from "@/features/User";
+import dynamic from "next/dynamic";
+
+const Login = dynamic(()=>import("../../features/User/container/Login"),{
+	loading:()=><User.Component.LoadingComponent/>
+})
+
+const Register = dynamic(()=>import("../../features/User/container/Register"),{
+	loading:()=><User.Component.LoadingComponent/>
+})
+
 
 export default function Signin() {
 	const [redirect, setRedirect] = useState(false);
@@ -12,9 +22,9 @@ export default function Signin() {
 			</Head>
 			<main>
 				{redirect ? (
-					<User.Container.RegisterContainer setRedirect={setRedirect} />
+					<Register setRedirect={setRedirect} />
 				) : (
-					<User.Container.LoginContainer setRedirect={setRedirect} />
+					<Login setRedirect={setRedirect} />
 				)}
 			</main>
 		</>
