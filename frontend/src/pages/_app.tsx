7d155cbfc462984e97f299d3614dfb50f4c3ce77
Mir Sahib <mirsahib24@gmail.com>
2023-05-26 23:06:02 +0600

    :recycle: Move font import to global css

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -3,20 +3,20 @@ import store from "@/store";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
-import { Comme } from "next/font/google";
+// import { Comme } from "next/font/google";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import AuthProvider from "@/context/AuthContext";
 config.autoAddCss = false;
 
-const comme = Comme({ subsets: ["latin"] });
+// const comme = Comme({ subsets: ["latin"] ,display:'swap'});
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<Provider store={store}>
 			<AuthProvider>
 				<MainLayout>
-					<main className={comme.className}>
+					<main >
 						<Component {...pageProps} />
 					</main>
 				</MainLayout>
